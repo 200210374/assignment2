117cb740af503c849af25afd28e04315b2c8a14e
brandonroycstn <brandonroycstn@hotmail.com>
2016-11-17 18:39:20 -0600

    connected routes for the views and they are in teh index.js file

--- assignment2/routes/index.js
+++ assignment2/routes/index.js
@@ -6,4 +6,25 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'contact' });
+});
+
+router.get('/directory', function(req, res, next) {
+  res.render('directory', { title: 'directory' });
+});
+
+router.get('/services', function(req, res, next) {
+  res.render('services', { title: 'services' });
+});
+
+/*
+router.get('/home', function(req, res, next) {
+  res.render('home', { title: 'home' });
+}); */
+
+
+
+
 module.exports = router;
