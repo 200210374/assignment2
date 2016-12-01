3f57dc57af69567df24a9f4160c1f2cd5a9a7d82
brandonroycstn <brandonroycstn@hotmail.com>
2016-11-30 20:19:43 -0600

    Changed index.js and other routes, created config folder and got index to use account model

--- assignment2/routes/index.js
+++ assignment2/routes/index.js
@@ -1,6 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+
+
+
+
+
+var Account = require('../models/account');
+//remember to install connect
+
+var passport = require('passport');
+var flash = require('connect-flash');
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -44,6 +56,69 @@ router.get('/registration', function(req, res, next) {
 router.get('/add-directory', function(req, res, next) {
   res.render('add-directory', { title: 'add-directory' });
 });
+
+
+
+
+router.post('/register', function(req, res, next) {
+  // use the Account model and passort to create a new user
+  Account.register(new Account(
+      { username: req.body.username }),
+      req.body.password,
+      function(err, account) {
+        if (err) {
+          console.log(err);
+          res.redirect('/register');
+        }
+        else {
+          res.redirect('/login');
+        }
+      });
+});
+
+/* GET login */
+router.get('/login', function(req, res, next) {
+
+  var messages = req.session.messages || []; //flash.message;
+
+  // clear the session messages
+  req.session.messages = [];
+
+  res.render('login', {
+    title: 'Login',
+    messages: messages,
+    user: req.user
+  });
+});
+
+
+router.post('/login', passport.authenticate('local', {
+  successRedirect: '/directory',
+  failureRedirect: '/login',
+  failureMessage: 'Invalid Login',
+  failureFlash: true
+}));
+
+
+router.get('/logout', function(req, res, next) {
+
+  req.logout();
+  res.redirect('/login');
+});
+
+
+router.get('/facebook', passport.authenticate('facebook'), function(req, res, next) {});
+
+router.get('/facebook/callback', passport.authenticate('facebook', {
+  failureRedirect: '/login',
+  failureMessage: 'Invalid Login'
+}), function(req, res, next){
+  res.redirect('/directory');
+});
+
+
+
+
 
 
 
